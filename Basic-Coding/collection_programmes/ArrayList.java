f8d1bb87045bb6f5a947b7a80e856f6cae3cc2e3
Mohammed Zakria Khan <mohammedzakriakhanz@gmai.com>
2023-04-04 00:18:18 +0530

    first commit

---
+++ Basic-Coding/collection_programmes/ArrayList.java
@@ -0,0 +1,22 @@
+
+package collection_programmes;
+import java.util.*;
+public class ArrayList
+{
+    public static void main(String[] args) {
+//       ArrayList<> al = new ArrayList<>();
+//       al.add(5);
+//       al.add(7);
+//       al.add(10);
+//
+//       if(al.contains(10))
+//       {
+//           System.out.println("10 is present");
+//       }
+//       else
+//       {
+//           System.out.println("Not present");
+//       }
+
+    }
+}
